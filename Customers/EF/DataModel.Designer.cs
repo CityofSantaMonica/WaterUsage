82dcdb01675d9da7007819b0860ad1a07db7e1fe
Kevin Hobson <kevin.hobson@smgov.net>
2022-04-20 11:36:13 -0700

    update to TLS 1.2

--- Customers/EF/DataModel.Designer.cs
+++ Customers/EF/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\git\Socrata\WaterUsage\Customers\EF\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\github\CityofSantaMonica\WaterUsage\Customers\EF\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
